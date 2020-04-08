e6d75601400063c12eb389b2947beb505a516512
riko <ewidodo1998@gmail.com>
2020-04-08 15:47:18 -0700

    scenes implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,12 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        //display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        //launches next scene
+        this.scene.start("playScene");
+    }
 }
